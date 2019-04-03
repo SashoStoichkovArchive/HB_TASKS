c2dfd5b5f9bfefe8d6edd98fb6520c6f272abdda
SashoStoichkov <sashostoichkov@gmail.com>
2019-04-03 19:36:46 +0300

    add accept.py

---
+++ projects/week06/decorators/accept.py
@@ -0,0 +1,23 @@
+def accepts(*args):
+    def accepter(func):
+        def decorated(*arg):
+            for index, argv in enumerate(args):
+                if argv != type(arg[index]):
+                    raise TypeError("Argument {} of {} is not {}!".format(index+1, func.__name__, argv.__name__))
+        return decorated
+    return accepter
+
+@accepts(str)
+def say_hello(name):
+    return "Hello, I am {}".format(name)
+
+# say_hello("Pesho")
+# say_hello(1)
+
+@accepts(str, int)
+def deposit(name, money):
+    print("{} sends {} $!".format(name, money))
+    return True
+
+# deposit("Roza", 1)
+# deposit("Roza", "a")
