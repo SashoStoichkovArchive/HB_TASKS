d2ced7ea53468cde12a80727d2c29f1f0bb01db4
SashoStoichkov <sashostoichkov@gmail.com>
2019-04-03 21:02:00 +0300

    add log decorator

--- projects/week06/decorators/encrypt_and_log.py
+++ projects/week06/decorators/encrypt_and_log.py
@@ -1,6 +1,10 @@
+import datetime
+from functools import wraps
+
 def encrypt(num):
     def accepter(func):
-        def decorated():
+        @wraps(func)
+        def decorated(*args, **kwargs):
             encrypted_string = ""
             for ch in func():
                 if ch != " ":
@@ -12,6 +16,18 @@ def encrypt(num):
         return decorated
     return accepter
 
+def log(filename):
+    def accepter(func):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            file = open(filename, "a")
+            file.write("{} was called at {}\n".format(func.__name__, datetime.datetime.now()))
+            file.close()
+            return func(*args, **kwargs)
+        return decorated
+    return accepter
+
+@log("log.txt")
 @encrypt(2)
 def get_low():
     return "Get get get low"
