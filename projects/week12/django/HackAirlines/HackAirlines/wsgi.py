9de594e1b327d0f1c60aef7c1efeef62b868f527
SashoStoichkov <sashostoichkov@gmail.com>
2019-08-08 16:54:03 +0300

    Make some changes

--- projects/week12/django/HackAirlines/HackAirlines/wsgi.py
+++ projects/week12/django/HackAirlines/HackAirlines/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HackAirline project.
+WSGI config for HackAirlines project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HackAirline.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HackAirlines.settings')
 
 application = get_wsgi_application()
